a69ff06255b213d50a3b6a997b80d573d999098b
codeMonkey1306 <36426431+codeMonkey1306@users.noreply.github.com>
2018-11-07 16:54:06 -0600

    Grade score 1

---
+++ main.py
@@ -0,0 +1,15 @@
+print('Wow best thing ever')
+counter = 0
+score_total = 0
+test_score = 0
+
+while test_score != 999:
+    test_score = int(input("Enter test score: "))
+    if test_score >= 0 and test_score <= 100:
+        score_total += test_score
+        counter += 1
+
+Average_score = round(score_total / counter)
+
+print("Total score: " + str(score_total))
+print("Average score: " + str(Average_score))
